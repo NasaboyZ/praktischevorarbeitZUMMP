2553375d87a4feef38fafb3a20ad8f9a5aefdb5d
Josef <josefleite.00@hotmail.com>
2026-08-07 21:35:20 +0200

    chore:changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,   // listen on all interfaces so phone can connect via local IP
-    port: 5173,
-  },
-});
+    host: '0.0.0.0',
+    port: 5174,
+    strictPort: true
+  }
+})
